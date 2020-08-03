210d04d3ab02a53b9805c650ddfc1aca9858257c
RedB73ulL <57768773+RedB73ulL@users.noreply.github.com>
2020-08-03 19:15:29 +0300

    Методы для работы с вебдрайвером вынес в отдельный класс Application. В тесте избавились от тестового класса, и теперь тест содержит только фикстуру, и тесты в которые фикстура передается как параметр.

---
+++ test_add_group_without_application.py
@@ -0,0 +1,22 @@
+from group import Group
+from application import Application
+import pytest
+
+# @ pytest.fixture - для pytest обозначает функцию, инициализирующую фикстуру
+@pytest.fixture
+def app(request):
+  # инициализация фикстуры
+  fixture = Application()
+  # указание на то, как фикстура должна быть разрушена
+  request.addfinalizer(fixture.destroy)
+  return fixture
+
+def test_add_group(app):
+  app.login(username="admin", password="secret")
+  app.create_group(Group(name="group", header="gh", footer="gf"))
+  app.logout()
+
+def test_add_empty_group(app):
+  app.login(username="admin", password="secret")
+  app.create_group(Group(name="", header="", footer=""))
+  app.logout()
