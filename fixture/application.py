8ce4493bef0c2995895969ac59502dd616505c7a
RedB73ulL <57768773+RedB73ulL@users.noreply.github.com>
2020-08-04 19:37:02 +0300

    Выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,39 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 class Application():
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         self.driver.get("http://localhost/addressbook/")
-
-    def open_groups_page(self):
-        self.driver.find_element(By.LINK_TEXT, "groups").click()
-
-    def create_group(self, group):
-        # init group creation
-        self.open_groups_page()
-        self.driver.find_element(By.NAME, "new").click()
-        # fill group form
-        self.driver.find_element(By.NAME, "group_name").click()
-        self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
-        self.driver.find_element(By.NAME, "group_header").click()
-        self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
-        self.driver.find_element(By.NAME, "group_footer").click()
-        self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        # submit group creation
-        self.driver.find_element(By.NAME, "submit").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        self.driver.find_element(By.LINK_TEXT, "group page").click()
-
-    def teardown_method(self):
-        self.driver.quit()
 
     def destroy(self):
         self.driver.quit()
