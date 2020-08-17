c471c4c7019a595cf694bbb48bdc15ac5cb6f137
RedB73ulL <57768773+RedB73ulL@users.noreply.github.com>
2020-08-17 14:12:24 +0300

    Добавил контактхелпер и новые тесты для контактов

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 class Application():
     def __init__(self):
@@ -8,6 +9,7 @@ class Application():
         self.driver.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         self.driver.get("http://localhost/addressbook/")
