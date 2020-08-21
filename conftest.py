993c544332ac14fdbf482fb1f7f77940d0092c27
RedB73ulL <57768773+RedB73ulL@users.noreply.github.com>
2020-08-21 12:44:21 +0300

    Теперь есть файл таргет джейсон, где можно настраивать параметры для запуска тестов

--- conftest.py
+++ conftest.py
@@ -1,20 +1,26 @@
 from fixture.application import Application
 import pytest
+import json
+import os.path
 
 fixture = None
+target = None
 
 # @ pytest.fixture - для pytest обозначает функцию, инициализирующую фикстуру
 @pytest.fixture #(scope="session")  # параметр для запуска фикстуры в кадом тесте/в сессии
 def app(request):
   global fixture
+  global target
   browser = request.config.getoption("--browser")
-  base_url = request.config.getoption("--baseUrl")
-  if fixture is None:
+  if target is None:
+    # прикрепляем путь к джейсону(определяем дерикторию в которой файл находится(преобразуем путь в абсолютн(__file__)))
+    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+    with open(config_file) as f:
+      target = json.load(f)
+  if fixture is None or not fixture.is_valid():
     # инициализация фикстуры
-    fixture = Application(browser=browser, base_url=base_url)
-  elif not fixture.is_valid():
-    fixture = Application(browser=browser, base_url=base_url)
-  fixture.session.ensure_login(username="admin", password="secret")
+    fixture = Application(browser=browser, base_url=target['baseUrl'])
+  fixture.session.ensure_login(username=target['username'], password=target['password'])
   return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -29,4 +35,4 @@ def stop(request):
 # функция для добавления собственных параметров запуска тестов
 def pytest_addoption(parser):
   parser.addoption("--browser", action="store", default="chrome")
-  parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+  parser.addoption("--target", action="store", default="target.json")
