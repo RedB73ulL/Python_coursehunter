8a6321d4f5d39aa4d0b7a292985c7564787ecd0e
RedB73ulL <57768773+RedB73ulL@users.noreply.github.com>
2020-08-25 18:28:09 +0300

    подготовка к загрузке данных из ДБ

--- conftest.py
+++ conftest.py
@@ -4,25 +4,32 @@ import json
 import os.path
 import importlib
 import jsonpickle
+from fixture.db import DbFixture
 
 fixture = None
 target = None
+
+#34
+def load_config(file):
+  global target
+  if target is None:
+    # прикрепляем путь к джейсону(определяем дерикторию в которой файл находится(преобразуем путь в абсолютн(__file__)))
+    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+    with open(config_file) as f:
+      target = json.load(f)
+  return target
 
 # @ pytest.fixture - для pytest обозначает функцию, инициализирующую фикстуру
 @pytest.fixture #(scope="session")  # параметр для запуска фикстуры в кадом тесте/в сессии
 def app(request):
   global fixture
-  global target
   browser = request.config.getoption("--browser")
-  if target is None:
-    # прикрепляем путь к джейсону(определяем дерикторию в которой файл находится(преобразуем путь в абсолютн(__file__)))
-    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-    with open(config_file) as f:
-      target = json.load(f)
+  #34
+  web_config = load_config(request.config.getoption("--target"))["web"]
   if fixture is None or not fixture.is_valid():
     # инициализация фикстуры
-    fixture = Application(browser=browser, base_url=target['baseUrl'])
-  fixture.session.ensure_login(username=target['username'], password=target['password'])
+    fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+  fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
   return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -57,4 +64,14 @@ def load_form_module(module):
 #32
 def load_form_json(file):
   with open (os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-    return jsonpickle.decode(f.read())\
+    return jsonpickle.decode(f.read())
+
+#34
+@pytest.fixture(scope="session")
+def db(request):
+  db_config = load_config(request.config.getoption("--target"))["db"]
+  dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+  def fin():
+    dbfixture.destroy()
+  request.addfinalizer(fin)
+  return dbfixture
